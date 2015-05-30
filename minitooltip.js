3a0792e1648f9989ca949490f3aa6c8bf8821511
Leonardo Camelo <leonardocamelo.nave@gmail.com>
2015-05-30 12:14:12 -0300

    mvp ready

--- minitooltip.js
+++ minitooltip.js
@@ -1,13 +1,29 @@
 (function(){
+
+  // search all minitooltips enabled
   var tooltips = [];
   var all = document.getElementsByTagName('*');
   for(var i = 0; i < all.length; i++){
     if(all[i].getAttribute('data-tip') !== null)
     tooltips.push(all[i]);
   }
-  tooltips.forEach(function(tooltip){
-    tooltip.addEventListener('mouseover', function(){
-      console.log(this.dataset.tip);
+
+  // create the tip element
+  var tip = document.createElement('div');
+  tip.id = 'minitooltip';
+  document.body.appendChild(tip);
+
+  // add events to show/hide tips
+  tooltips.forEach(function(tooltip){  
+    tooltip.addEventListener('mouseenter', function(){
+      tip.textContent = this.dataset.tip;
+      tip.style.display = 'block';
+      tip.style.top = this.offsetTop - 40 + 'px';
+      tip.style.left = (this.offsetLeft + this.offsetWidth / 2) - tip.offsetWidth / 2 + 'px';
+    });
+    tooltip.addEventListener('mouseleave', function(){
+      tip.style.display = 'none';
     });
   });
+
 }).call(this);
