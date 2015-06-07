91a6fd9ab2853535f462b4518462e7754c56f72b
Leonardo Camelo <leonardocamelo.nave@gmail.com>
2015-06-07 13:53:30 -0300

    add in js stylesheet

--- minitooltip.js
+++ minitooltip.js
@@ -1,16 +1,26 @@
 (function(){
 
-  // search all minitooltips enabled
-  var tooltips = [];
-  var all = document.getElementsByTagName('*');
+  // search all minitooltips elements
+  var tooltips = [],
+  all = document.getElementsByTagName('*');
   for(var i = 0; i < all.length; i++){
     if(all[i].getAttribute('data-tip') !== null)
     tooltips.push(all[i]);
   }
 
-  // create the tip element
-  var tip = document.createElement('div');
+  // create the tip element and her style
+  var tip = document.createElement('span'), style = document.createElement('style'),
+  css = '#minitooltip{display:none;position:fixed;z-index:9;pointer-events:none;color:#fff;'
+  +'background-color:#333;padding:8px;font-family:sans-serif;font-size:.8em;font-weight:lighter;'
+  +'border-radius:2px;-webkit-border-radius:2px;-moz-border-radius:2px;}#minitooltip:after{content:"";'
+  +'width:0;height:0;position:absolute;border-top:8px #333 solid;border-left:8px transparent solid;'
+  +'border-right:8px transparent solid;top:100%;left:50%;margin-left:-8px;}';
   tip.id = 'minitooltip';
+  style.type = 'text/css';
+  style.appendChild(document.createTextNode(css));
+
+  // append tip and style to document
+  document.head.appendChild(style);
   document.body.appendChild(tip);
 
   // add events to show/hide tips
