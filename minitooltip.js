d210520ee841631391d32025e646b079a9d8b795
Somar 5 <leonardo.camelo@somarcomunicacao.com.br>
2015-07-23 19:09:24 -0300

    assign event to document

--- minitooltip.js
+++ minitooltip.js
@@ -1,13 +1,5 @@
 (function(){
 
-  // search all minitooltips elements
-  var all = document.getElementsByTagName('*'),
-  length = all.length, tooltips = [];
-  for(var i = 0; i < length; i++){
-    if(all[i].getAttribute('data-tip'))
-    tooltips.push(all[i]);
-  }
-
   // create the tip element and her style
   var tip = document.createElement('div'), style = document.createElement('style'),
   css = '#tip{display:block;opacity:0;position:absolute;z-index:9999;color:#fff;text-align:center;'
@@ -24,36 +16,39 @@
   document.head.appendChild(style);
   document.body.appendChild(tip);
 
-  // add events to show/hide tips
-  tooltips.forEach(function(tooltip){
-    tooltip.onmouseover = function(){
-      // set the content of tip and show it
-      tip.textContent = this.dataset.tip;
+  // add events to show tips
+  document.onmouseover = function(e){
 
-      // suport to positions
-      var position = 'u', top,
-      dataP = this.dataset.tipPosition,
-      rect = this.getBoundingClientRect();
-      if(dataP && ['u', 'd'].indexOf(dataP.charAt(0)) != -1)
-      position = dataP.charAt(0);
-      else if(rect.top - 40 <= 0)
-      position = 'd';
-      if(position == 'u')
-      top = rect.top + window.scrollY - tip.offsetHeight - 9 + 'px';
-      else if(position == 'd')
-      top = rect.top + window.scrollY + rect.height + 9 + 'px';
-      tip.style.top = top;
-      tip.dataset.p = position;
-
-      // align horizontal
-      tip.style.left = (rect.left + rect.width / 2) - tip.offsetWidth / 2 + 'px';
-
-      // show it!
-      tip.style.opacity = 1;
-    };
-    tooltip.onmouseout = function(){
+    // check if the element is a tooltip
+    var tooltip = e.target;
+    if(tooltip.getAttribute('data-tip') == null){
       tip.style.opacity = 0;
-    };
-  });
+      return false;
+    }
+
+    // set the content of tip and show it
+    tip.textContent = tooltip.dataset.tip;
+
+    // suport to positions
+    var position = 'u', top,
+    dataP = tooltip.dataset.tipPosition,
+    rect = tooltip.getBoundingClientRect();
+    if(dataP && ['u', 'd'].indexOf(dataP.charAt(0)) != -1)
+    position = dataP.charAt(0);
+    else if(rect.top - 40 <= 0)
+    position = 'd';
+    if(position == 'u')
+    top = rect.top + window.scrollY - tip.offsetHeight - 9 + 'px';
+    else if(position == 'd')
+    top = rect.top + window.scrollY + rect.height + 9 + 'px';
+    tip.style.top = top;
+    tip.dataset.p = position;
+
+    // align horizontal
+    tip.style.left = (rect.left + rect.width / 2) - tip.offsetWidth / 2 + 'px';
+
+    // show it!
+    tip.style.opacity = 1;
+  };
 
 })();
